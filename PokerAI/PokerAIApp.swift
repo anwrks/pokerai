7e5ee413cc4cdcdd9a89c5f44ac55c181513305b
Chris Mullins <anwrks@gmail.com>
2026-01-25 00:23:39 -0500

    Initial Commit

---
+++ PokerAI/PokerAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerAIApp.swift
+//  PokerAI
+//
+//  Created by Chris Mullins on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct PokerAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
